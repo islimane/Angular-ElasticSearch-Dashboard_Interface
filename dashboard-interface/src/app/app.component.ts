30bc75efd4fe01d3750929283c516d324628c1b8
islimane <isma.bled777@gmail.com>
2017-04-28 21:44:29 +0200

    Tried to ping elastucsearch cluster, but it fails trying to reach it

--- dashboard-interface/src/app/app.component.ts
+++ dashboard-interface/src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
+import { DataService } from './data.service';
+import { Elasticsearch } from './elasticsearch';
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+	title: string;
+
+	constructor(
+		private dataService: DataService,
+		public elasticsearch: Elasticsearch
+	) { }
+
+	ngOnInit(): void {
+		this.getTitle();
+		/*this.elasticsearch
+			.test_search()
+			.subscribe((results) => {
+				console.log(results);
+			});*/
+		console.log(this.elasticsearch);
+		this.elasticsearch.ping();
+	}
+
+	getTitle(): void {
+		console.log('HOLA!');
+		this.title = this.dataService.getTitle();
+	}
 }
