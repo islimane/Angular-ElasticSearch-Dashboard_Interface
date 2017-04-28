b3c158232d727eb20ac5cee84f73ad46a09ffe67
islimane <isma.bled777@gmail.com>
2017-04-28 21:43:13 +0200

    Included services on providers

--- dashboard-interface/src/app/app.module.ts
+++ dashboard-interface/src/app/app.module.ts
@@ -4,17 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { DataService } from './data.service';
+import { Elasticsearch } from './elasticsearch';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		FormsModule,
+		HttpModule
+	],
+	providers: [
+		DataService,
+		Elasticsearch
+	],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
