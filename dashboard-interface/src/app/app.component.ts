d176eccecc4b32059e2488a951ea3f030d618c47
islimane <isma.bled777@gmail.com>
2017-05-13 15:17:02 +0200

    Deleted useless comment.

--- dashboard-interface/src/app/app.component.ts
+++ dashboard-interface/src/app/app.component.ts
@@ -19,22 +19,6 @@ export class AppComponent {
 
 	ngOnInit(): void {
 		this.getTitle();
-
-		/*this.elasticsearch.clientElasticsearch.count(
-			{
-				index: 'bank'
-			},
-			function (error, response) {
-				if (error) {
-					console.error('elasticsearch cluster is down!');
-					console.error(error);
-				} else {
-					console.log(response.count);
-				}
-			}
-		);*/
-
-
 	}
 
 	getTitle(): void {
