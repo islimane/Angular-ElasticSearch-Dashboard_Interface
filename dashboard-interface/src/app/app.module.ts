fbba9476ee7f130d44347cedc6a0e1bd63288d9d
islimane <isma.bled777@gmail.com>
2017-10-14 13:57:35 +0200

    Deleted unnecesary components and modules.

--- dashboard-interface/src/app/app.module.ts
+++ dashboard-interface/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from "@angular/flex-layout";
 
 import { AppComponent } from './app.component';
@@ -29,47 +28,7 @@ import { HiddenDirective } from './shared/hidden.directive';
 import { Elasticsearch } from './elasticsearch';
 import { ChartsModule } from 'ng2-charts/ng2-charts';
 
-import { MissionControlComponent } from './example/missioncontrol.component';
-import { AstronautComponent } from './example/astronaut.component';
-import { MissionService } from './example/mission.service';
-
 import { Collapse } from './shared/collapse.directive';
-
-// Angular Material
-import {
-	MdMenuModule,
-//	CdkTableModule,
-	MdAutocompleteModule,
-	MdButtonModule,
-	MdButtonToggleModule,
-	MdCardModule,
-	MdCheckboxModule,
-	MdChipsModule,
-	MdCoreModule,
-	MdDatepickerModule,
-	MdDialogModule,
-	MdExpansionModule,
-	MdGridListModule,
-	MdIconModule,
-	MdInputModule,
-	MdListModule,
-	MdNativeDateModule,
-	MdPaginatorModule,
-	MdProgressBarModule,
-	MdProgressSpinnerModule,
-	MdRadioModule,
-	MdRippleModule,
-	MdSelectModule,
-	MdSidenavModule,
-	MdSliderModule,
-	MdSlideToggleModule,
-	MdSnackBarModule,
-	MdSortModule,
-	MdTableModule,
-	MdTabsModule,
-	MdToolbarModule,
-	MdTooltipModule
-} from '@angular/material';
 
 @NgModule({
 	declarations: [
@@ -86,61 +45,22 @@ import {
 		BarChartComponent,
 		DynamicComponent,
 		HiddenDirective,
-		MissionControlComponent,
-		AstronautComponent,
 		BucketsComponent,
 		BucketComponent,
 		Collapse
 	],
 	imports: [
-		BrowserModule,
-		FormsModule,
-		ReactiveFormsModule,
-		HttpModule,
-		ChartsModule,
-		BrowserAnimationsModule,
-		MdMenuModule,
-		MdAutocompleteModule,
-		MdButtonModule,
-		MdButtonToggleModule,
-		MdCardModule,
-		MdCheckboxModule,
-		MdChipsModule,
-		MdCoreModule,
-		MdDatepickerModule,
-		MdDialogModule,
-		MdExpansionModule,
-		MdGridListModule,
-		MdIconModule,
-		MdInputModule,
-		MdListModule,
-		MdMenuModule,
-		MdNativeDateModule,
-		MdPaginatorModule,
-		MdProgressBarModule,
-		MdProgressSpinnerModule,
-		MdRadioModule,
-		MdRippleModule,
-		MdSelectModule,
-		MdSidenavModule,
-		MdSliderModule,
-		MdSlideToggleModule,
-		MdSnackBarModule,
-		MdSortModule,
-		MdTableModule,
-		MdTabsModule,
-		MdToolbarModule,
-		MdTooltipModule,
 		FlexLayoutModule
 	],
 	providers: [
 		DataService,
 		Elasticsearch,
 		MetricsService,
-		VisualizationsService,
-		MissionService
+		VisualizationsService
 	],
-	bootstrap: [AppComponent],
+	bootstrap: [
+		AppComponent
+	],
 	entryComponents: [
 		MetricsComponent,
 		DataTableComponent,
